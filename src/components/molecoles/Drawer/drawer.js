96afa07663e4a3d8bd40d0a031d15f56a351bd78
danilloromano <danilloferomano@gmail.com>
2020-10-28 01:25:13 -0300

    adding resizeble property on drawer

--- src/components/molecoles/Drawer/drawer.js
+++ src/components/molecoles/Drawer/drawer.js
@@ -1,36 +1,59 @@
-import React from 'react';
-import clsx from 'clsx';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import IconButton from '@material-ui/core/IconButton';
+import React, { useCallback, useState } from 'react'
+import clsx from 'clsx'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import IconButton from '@material-ui/core/IconButton'
 
 import drawerStyles from './drawer-style'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 import { updateDrawerState } from '../../../redux/actions/actions'
 
-const MyDrawer = ({drawerState, updateDrawerState}) => {
+const MyDrawer = ({ drawerState, updateDrawerState }) => {
 
-    const classes = drawerStyles();
+    const classes = drawerStyles()
+    const defaultDrawerWidth = '200px'
+    const [drawerWidth, setDrawerWidth] = useState(defaultDrawerWidth);
+    const minDrawerWidth = 50;
+    const maxDrawerWidth = 300;
+
+    const handleMouseDown = e => {
+        document.addEventListener("mouseup", handleMouseUp, true);
+        document.addEventListener("mousemove", handleMouseMove, true);
+    };
+
+    const handleMouseUp = () => {
+        document.removeEventListener("mouseup", handleMouseUp, true);
+        document.removeEventListener("mousemove", handleMouseMove, true);
+    };
+
+    const handleMouseMove = useCallback(e => {
+        const newWidth = e.clientX - document.body.offsetLeft;
+        if (newWidth > minDrawerWidth && newWidth < maxDrawerWidth) {
+            setDrawerWidth(newWidth);
+        }
+    }, []);
 
     return (
         <Drawer
             className={clsx(classes.drawerClosed, drawerState && classes.drawerOppened)}
             variant="persistent"
-            classes={{paper: classes.drawerPaper}}
+            classes={{ paper: classes.drawerPaper }}
             anchor="left"
             open={drawerState}
-            ModalProps={{
-                keepMounted: true // Better open performance on mobile.
-            }}
-            PaperProps={{  }}
+            PaperProps={{ style: { width: drawerWidth } }}
         >
+            <div
+                id="dragger"
+                onMouseDown={e => handleMouseDown(e)}
+                className={classes.dragger}
+            />
             <div className={classes.drawerHeader}>
                 <IconButton onClick={() => updateDrawerState(false)}>
                     <ChevronLeftIcon />
